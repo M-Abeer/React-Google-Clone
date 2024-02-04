7841ab7c792b9a2bd58cbf357e0ffd9d884350e6
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-02-04 22:21:41 +0500

    Googl

---
+++ src/Context/ResultContextProvider.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = `https://google-search74.p.rapidapi.com/`;
+const ResultContextProvider = ({ children }) => {
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchterm, setSearchTerm] = useState(" ");
+
+  // /videos, /search,.images
+  const getResults = async (type) => {
+    setLoading(true);
+    const response = await fetch(`${baseUrl}${type},{
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'a35c8af98cmshf99f31962e4a8f1p11c47cjsn0bb4134c244c',
+            'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+          }
+    }`);
+    const data = await response.json();
+    setResult(data);
+    setLoading(false);
+  };
+  return (
+    <>
+      <ResultContext.Provider
+        value={{ getResults, result, searchterm, setSearchTerm, loading }}
+      >
+        {children}
+      </ResultContext.Provider>
+    </>
+  );
+};
+
+const useResultContext = () => {
+  useContext(ResultContext);
+};
+
+export default ResultContextProvider;
